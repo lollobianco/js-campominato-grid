7030872125756a2ad5d8efc61848f8f16f308d81
lollobianco <loreb2002@gmail.com>
2022-10-28 15:01:13 +0200

    Creata funzione che crea i box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
+let boxContainer = document.getElementById('boxContainer');
 
+let playButton = document.getElementById('playButton');
+
+
+//Funzione che crea le box al click del tasto play
+
+playButton.addEventListener('click', function(){
+
+   for (let i = 1; i <= 100; i++) {
+   
+      let box = document.createElement("div");
+
+      box.innerText = `${i}`
+
+      box.classList.add('box', 'd-flex', 'align-items-center', 'justify-content-center', 'fs-5');
+
+      boxContainer.append(box);
+
+   }
+
+});
