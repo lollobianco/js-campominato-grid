5daa221fdae30cf8acff4fbb235b24e3f4aab2d5
lollobianco <loreb2002@gmail.com>
2022-10-28 16:08:05 +0200

    Esercizio base completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,39 @@ let boxContainer = document.getElementById('boxContainer');
 let playButton = document.getElementById('playButton');
 
 
+
+//Funzione che crea le box
+
+function createBox(){
+
+   let box = document.createElement('div');
+   box.classList.add('box', 'fs-5');
+   return box;
+
+}
+
+
 //Funzione che crea le box al click del tasto play
 
 playButton.addEventListener('click', function(){
 
    for (let i = 1; i <= 100; i++) {
-   
-      let box = document.createElement("div");
 
-      box.innerText = `${i}`
+      let box = createBox();
 
-      box.classList.add('box', 'd-flex', 'align-items-center', 'justify-content-center', 'fs-5');
+      box.innerText = `${i}`;
 
       boxContainer.append(box);
+      
+      box.addEventListener('click', function(){
+
+         console.log(this.innerText);
+
+         this.classList.toggle('boxClick');
+   
+      })
 
    }
 
 });
+
